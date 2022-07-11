9f55c06d427a1950e886c1c5a54166e0ad068f24
Travis Gersch <t_gersch@hotmail.com>
2022-07-11 02:12:29 -0500

    methods overload exercise initial commit

---
+++ Methods_Overload_Exercise/Methods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Overload_Exercise;
+
+internal class Methods
+{   
+    public static int Add(int add1, int add2)
+    {
+        return add1 + add2;
+    }
+
+    public static decimal Add(decimal add1, decimal add2)
+    {
+        return add1 + add2;
+    }
+
+    public static string Add(int add1, int add2, bool isTrue)
+    {
+        var total = add1 + add2;
+
+        if (isTrue == true)
+        {
+            if (total == 1)
+            {
+                return $"{total} dollar";
+            }
+            else if (total > 1 || total == 0)
+            {
+                return $"{total} dollars";
+            }
+            //else if (total > 0 && total < 1)  //commented this part out for the time being because I couldn't wrap my head around
+            //{                                //allowing inputs for both integers and decimals in the same method                
+            //    return $"{total} cents";    //without having to make another method...unless that's the solution
+            //}
+            
+        }   return total.ToString();
+    }
+
+}
---
+++ Methods_Overload_Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using Methods_Overload_Exercise;
+
+var a = 12;
+var b = 34;
+var intAnswer = Methods.Add(a, b);
+
+var c = 12.3m;
+var d = 45.6m;
+var decAnswer = Methods.Add(c, d);
+
+string mixedAnswer = Methods.Add(0, 1, true);
+
+Console.WriteLine(intAnswer);
+Console.WriteLine();
+Console.WriteLine(decAnswer);
+Console.WriteLine();
+Console.WriteLine(mixedAnswer);
